8bbeea8e574520bbbe93691690f7931694df037c
Robert Pincus <Robert.Pincus@colorado.edu>
2020-10-01 15:56:26 -0400

    Using flight-level data from P3 catalog (PSL server); dates() instead of strings for flight dates

--- scripts/expand-yaml.py
+++ scripts/expand-yaml.py
@@ -14,13 +14,16 @@
 import datetime
 import yaml
 import pathlib
+from intake import open_catalog
 
 import xarray as xr
 
-data_dir = pathlib.Path('/Users/robert/Dropbox/Scientific/Projects/ATOMIC:EURECA4/data')
+cat_dir  = pathlib.Path('/Users/robert/Dropbox/Scientific/Papers/ATOMIC-P3-data/Figures/p3-intake-catalog/')
 seg_dir  = pathlib.Path('./flight_phase_files')
 mini_dir = pathlib.Path('./mini-yaml')
 
+campaign = "EUREC4A"
+project  = "ATOMIC"
 platform = "P3"
 variable = "Flight-segments"
 data_version = "v0.5"
@@ -32,12 +35,20 @@ seg_types = ['transit', 'circle', 'profile', 'axbt', 'cloud']
 #
 # .
 #
-flight_dates = ['2020-01-17', '2020-01-19', '2020-01-23',
-                '2020-01-24', '2020-01-31',
-                '2020-02-03', '2020-02-04', '2020-02-05',
-                '2020-02-09', '2020-02-10', '2020-02-11']
+flight_dates = [datetime.date(2020, 1, 17),
+                datetime.date(2020, 1, 19),
+                datetime.date(2020, 1, 23),
+                datetime.date(2020, 1, 24),
+                datetime.date(2020, 1, 31),
+                datetime.date(2020, 2,  3),
+                datetime.date(2020, 2,  4),
+                datetime.date(2020, 2,  5),
+                datetime.date(2020, 2,  9),
+                datetime.date(2020, 2, 10),
+                datetime.date(2020, 2, 11)]
 
-p3_info = {'campaign':'EUREC4A', 'activity':'ATOMIC', 'platform':'P3',
+
+p3_info = {'campaign':'EUREC4A', 'project':'ATOMIC', 'platform':'P3',
            "contacts": [{'name': 'Robert Pincus',
                         'email': 'Robert.Pincus@colorado.edu',
                         'tags': ['sc', 'dp']},
@@ -48,24 +59,20 @@ p3_info = {'campaign':'EUREC4A', 'activity':'ATOMIC', 'platform':'P3',
                         'email': 'Chris.Fairall@noaa.gov',
                         'tags': ['cs', 'pi']}] }
 
-def dict_to_datetime(year, month, day, tm):
+def date_to_datetime(d, tm):
     # '''Here tm is a dict with keys hour, min, and optinally sec'''
     if 'sec' in tm.keys():
-        return(datetime.datetime(year, month, day, hour=int(tm["hour"]), minute=int(tm["min"]), second=int(tm["sec"])))
+        return(datetime.datetime(d.year, d.month, d.day, hour=int(tm["hour"]), minute=int(tm["min"]), second=int(tm["sec"])))
     else:
-        return(datetime.datetime(year, month, day, hour=int(tm["hour"]), minute=int(tm["min"])))
+        return(datetime.datetime(d.year, d.month, d.day, hour=int(tm["hour"]), minute=int(tm["min"])))
 
 if __name__ == "__main__":
+    p3data = open_catalog(str(cat_dir.joinpath('main.yaml')))
     for d in flight_dates:
-        year  = int(d[0:4])
-        month = int(d[5:7])
-        day   = int(d[8:10])
         #
         # Flight level summary file - could construct name/URL more programatically
         #
-        fl_file = sorted(data_dir.joinpath("flight-level-summary/Level_2").glob("*" + d.replace('-', '')  + "*.nc"))[0]
-        with fl_file as flight_level:
-            f = xr.open_dataset(flight_level)
+        f = p3data.flight_level["P3-{:02d}{:02d}".format(d.month,d.day)].to_dask()
         #
         # Take-off and landing times, approach suggested by Adriana Bailey
         #
@@ -74,9 +81,9 @@ if __name__ == "__main__":
         takeoff_time = in_air.time[ 0].values.astype('datetime64[s]').tolist()
         landing_time = in_air.time[-1].values.astype('datetime64[s]').tolist()
 
-        mini_files = sorted(mini_dir.glob("*" + d + "*.yaml"))
+        mini_files = sorted(mini_dir.glob("*" + "{:04d}-{:02d}-{:02d}".format(d.year,d.month,d.day) + "*.yaml"))
         if len(mini_files) is 0:
-            print ("Mini YAML file missing, skipping date " + d)
+            print ("Mini YAML file missing, skipping date " + "{:04d}-{:02d}-{:02d}".format(d.year,d.month,d.day))
         else:
             print('Flight RF{:02d}'.format(flight_dates.index(d) + 1))
             print("  nominal takeoff time", takeoff_time, "nominal landing time", landing_time)
@@ -94,7 +101,7 @@ if __name__ == "__main__":
                 #   check that the time is during the flight
                 #
                 for t in ["start", "end"]:
-                    seg[t] = dict_to_datetime(year, month, day, seg[t])
+                    seg[t] = date_to_datetime(d, seg[t])
                     # Here's where we should check against takeoff and landing times
                     if(seg[t] < takeoff_time or seg[t] > landing_time): print ("oh noes segment ", seg["name"], "seems has bad time")
                 #
@@ -106,7 +113,7 @@ if __name__ == "__main__":
                     seg["segment_id"] = "NULL"
                 else:
                     type_counts[seg["kind"]] += 1
-                    seg["segment_id"] = 'P3-{:02d}{:02d}_{}{}'.format(month, day, seg["kind"][0:2], type_counts[seg["kind"]])
+                    seg["segment_id"] = 'P3-{:02d}{:02d}_{}{}'.format(d.month, d.day, seg["kind"][0:2], type_counts[seg["kind"]])
                 #
                 # Rename one attrtibute
                 #
@@ -117,19 +124,19 @@ if __name__ == "__main__":
                 seg = {s:seg[s] for s in ["kinds", "name", "segment_id", "start", "end", "irregularities"] }
 
             flight_yaml = p3_info
-            flight_yaml.update({'flight_id': 'P3-{:02d}{:02d}'.format(month, day),
+            flight_yaml.update({'flight_id': 'P3-{:02d}{:02d}'.format(d.month, d.day),
                                 'name': 'RF{:02d}'.format(flight_dates.index(d) + 1),
                                 'events': [],
                                 'remarks': [],
-                                'date': datetime.date(year, month, day),
+                                'date': d,
                                 'flight_report': 'https://observations.ipsl.fr/aeris/eurec4a-data/REPORTS/WP-3D/2020/TO-COME.pdf',
                                 'takeoff' : takeoff_time,
                                 'landing' : landing_time,
                                 'version': data_version,
                                 'segments':[seg for seg in m]})
 
-            output_name = seg_dir.joinpath("{}_{}".format(platform, variable) +
-                                           "_{:04d}{:02d}{:02d}".format(year,month,day) + "_" + data_version + ".yaml")
+            output_name = seg_dir.joinpath("{}_{}_{}_{}".format(campaign, project, platform, variable) +
+                                           "_{:04d}{:02d}{:02d}".format(d.year,d.month,d.day) + "_" + data_version + ".yaml")
             print ("  Writing to ", output_name)
             with open(output_name, "w") as stream:
                 yaml.dump(flight_yaml, stream, default_flow_style=False, sort_keys=False)
