51d00072ccfc0f196376e900116412c2a6e67996
Robert Pincus <Robert.Pincus@colorado.edu>
2020-10-01 20:37:01 -0400

    Specify which yaml files to read; time formatting improvements; can write only only some segment types

--- scripts/expand-yaml.py
+++ scripts/expand-yaml.py
@@ -21,6 +21,8 @@ import xarray as xr
 cat_dir  = pathlib.Path('/Users/robert/Dropbox/Scientific/Papers/ATOMIC-P3-data/Figures/p3-intake-catalog/')
 seg_dir  = pathlib.Path('./flight_phase_files')
 mini_dir = pathlib.Path('./mini-yaml')
+mini_prefix = "flight-phase-mini"
+
 
 campaign = "EUREC4A"
 project  = "ATOMIC"
@@ -31,7 +33,8 @@ data_version = "v0.5"
 #
 # Controlled vocabulary for segment types - the script will complain if a segment has a different kind.
 #
-seg_types = ['transit', 'circle', 'profile', 'axbt', 'cloud']
+valid_seg_types = ['transit', 'circle', 'profile', 'axbt', 'cloud']
+include_seg_types = ['circle']
 #
 # .
 #
@@ -67,12 +70,13 @@ def date_to_datetime(d, tm):
         return(datetime.datetime(d.year, d.month, d.day, hour=int(tm["hour"]), minute=int(tm["min"])))
 
 if __name__ == "__main__":
+    print("Processing " + mini_prefix + " mini yaml files")
     p3data = open_catalog(str(cat_dir.joinpath('main.yaml')))
     for d in flight_dates:
         #
         # Flight level summary file - could construct name/URL more programatically
         #
-        f = p3data.flight_level["P3-{:02d}{:02d}".format(d.month,d.day)].to_dask()
+        f = p3data.flight_level[d.strftime("P3-%m%d")].to_dask()
         #
         # Take-off and landing times, approach suggested by Adriana Bailey
         #
@@ -81,9 +85,9 @@ if __name__ == "__main__":
         takeoff_time = in_air.time[ 0].values.astype('datetime64[s]').tolist()
         landing_time = in_air.time[-1].values.astype('datetime64[s]').tolist()
 
-        mini_files = sorted(mini_dir.glob("*" + "{:04d}-{:02d}-{:02d}".format(d.year,d.month,d.day) + "*.yaml"))
+        mini_files = sorted(mini_dir.glob(mini_prefix + d.strftime('-%Y-%m-%d') + "*.yaml"))
         if len(mini_files) is 0:
-            print ("Mini YAML file missing, skipping date " + "{:04d}-{:02d}-{:02d}".format(d.year,d.month,d.day))
+            print ("Mini YAML file missing, skipping date " + d.strftime('%Y-%m-%d'))
         else:
             print('Flight RF{:02d}'.format(flight_dates.index(d) + 1))
             print("  nominal takeoff time", takeoff_time, "nominal landing time", landing_time)
@@ -93,7 +97,7 @@ if __name__ == "__main__":
             # Number of segments of each type encountered so far
             #   Could discover from YAML file
             #
-            type_counts = {t:0 for t in seg_types}
+            type_counts = {t:0 for t in valid_seg_types}
             for seg in m:
                 seg["irregularities"] = []
                 #
@@ -108,7 +112,7 @@ if __name__ == "__main__":
                 # Segment ID is two characters for month, day; includes two character from type.
                 #  We're assuming that the segments come in sorted
                 #
-                if seg["kind"] not in seg_types:
+                if seg["kind"] not in valid_seg_types:
                     print ("oh noes I don't understand this segment type")
                     seg["segment_id"] = "NULL"
                 else:
@@ -124,7 +128,7 @@ if __name__ == "__main__":
                 seg = {s:seg[s] for s in ["kinds", "name", "segment_id", "start", "end", "irregularities"] }
 
             flight_yaml = p3_info
-            flight_yaml.update({'flight_id': 'P3-{:02d}{:02d}'.format(d.month, d.day),
+            flight_yaml.update({'flight_id': d.strftime("P3-%m%y"),
                                 'name': 'RF{:02d}'.format(flight_dates.index(d) + 1),
                                 'events': [],
                                 'remarks': [],
@@ -133,10 +137,10 @@ if __name__ == "__main__":
                                 'takeoff' : takeoff_time,
                                 'landing' : landing_time,
                                 'version': data_version,
-                                'segments':[seg for seg in m]})
+                                'segments':[seg for seg in m if seg["kinds"][0] in include_seg_types]})
 
             output_name = seg_dir.joinpath("{}_{}_{}_{}".format(campaign, project, platform, variable) +
-                                           "_{:04d}{:02d}{:02d}".format(d.year,d.month,d.day) + "_" + data_version + ".yaml")
+                                           d.strftime("_%Y%m%y") + "_" + data_version + ".yaml")
             print ("  Writing to ", output_name)
             with open(output_name, "w") as stream:
                 yaml.dump(flight_yaml, stream, default_flow_style=False, sort_keys=False)
